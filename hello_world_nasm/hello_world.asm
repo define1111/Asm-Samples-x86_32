d49199e5b4777200e54a6fcd9c1e370c13c99b49
define1111 <s.vershkov.def@yandex.ru>
2020-08-08 18:47:18 +0300

    added comments for hello world

--- hello_world_nasm/hello_world.asm
+++ hello_world_nasm/hello_world.asm
@@ -1,16 +1,17 @@
 global _start
 
 section .data
-hello_world_msg db  "Hello, World!", 10, 0
-hw_msg_len      equ $-hello_world_msg
+hello_world_msg db  "Hello, World!", 10, 0      ; It is not good to store a constant string in the data section, 
+                                                ; but for the first time so be it
+hw_msg_len      equ $-hello_world_msg           ; variable with string length
 
 section .text
-_start: mov     eax, 4
-        mov     ebx, 1
-        mov     ecx, hello_world_msg
-        mov     edx, hw_msg_len
-        int     80h
+_start: mov     eax, 4                          ; system call number (sys_write)
+        mov     ebx, 1                          ; file descriptor (stdout)
+        mov     ecx, hello_world_msg            ; pointer to string
+        mov     edx, hw_msg_len                 ; length of string
+        int     0x80                            ; legacy syscall in x86_32
 
-        mov     eax, 1
-        mov     ebx, 0
-        int     80h
+        mov     eax, 1                          ; (sys_exit)
+        mov     ebx, 0                          ; exit return code
+        int     0x80
