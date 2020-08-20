20c5b261ec0e64742e567296d4659708de4917a1
define1111 <s.vershkov.def@yandex.ru>
2020-08-20 15:07:06 +0300

    added comments to sum_while

--- sum_while_nasm/sum_while_fill_nasm.asm
+++ sum_while_nasm/sum_while_fill_nasm.asm
@@ -2,22 +2,21 @@ global _start
 
 section .text
 
-_start:     mov     ecx, 10
+_start:     mov     ecx, 10                             ; ecx = 10
 
-lp_fill:    mov     dword [array + 4*ecx - 4], ecx
-;            sub     dword [array + 4*ecx - 4], 1024
-            loop    lp_fill
+lp_fill:    mov     dword [array + 4*ecx - 4], ecx      ; array[ecx-1] = ecx
+            loop    lp_fill                             ; --ecx and if ecx != 0 go to lp_fill
 
-            mov     ecx, 10
-            xor     eax, eax
+            mov     ecx, 10                             ; ecx = 10
+            xor     eax, eax                            ; eax = 0
 
-lp_sum:     add     eax, dword [array + 4*ecx - 4]
-            loop    lp_sum    
+lp_sum:     add     eax, dword [array + 4*ecx - 4]      ; eax += array[ecx-1]
+            loop    lp_sum                              ; --ecx and if ecx != 0 go to lp_sum
           
-            mov     ebx, eax
-            mov     eax, 1
-            int     0x80
+            mov     ebx, eax                            ; ebx = sum of array
+            mov     eax, 1                              ; sys_exit
+            int     0x80                                ; syscall
 
 section .bss
 
-array       resd  10 
+array       resd  10                                    ; pointer to reserved 10 cells of 4-bytes cells
--- sum_while_nasm/sum_while_nasm.asm
+++ sum_while_nasm/sum_while_nasm.asm
@@ -2,14 +2,19 @@ global _start
 
 section .text
 
-array dw 1, 2, 3, 4, 5, 6, 7, 8, 9
+array dw 1, 2, 3, 4, 5, 6, 7, 8, 9                  ; constant array
 
-_start: mov     ecx, 9
-        xor     eax, eax 
+_start: mov     ecx, 9                              ; put length of array to ecx
+        xor     eax, eax                            ; eax = 0 (and ax = 0 too)
 
-lp:     add     ax, word [array + 2*ecx - 2]
-        loop    lp
+lp:     add     ax, word [array + 2*ecx - 2]        ; ax += array[ecx-1] // *(array + (ecx - 1))
+                                                    ; 2*ecx this coef needs becouze C compiler
+                                                    ; automatically calculates the offset of the array cell,
+                                                    ; and we are forced to multiply it explicity
 
-        mov     ebx, eax
-        mov     eax, 1
-        int     0x80
+        loop    lp                                  ; --ecx and if ecx != 0 go to lp
+
+        mov     ebx, eax                            ; summ of array go to return exit code
+                                                    ; (check it by command $ echo $?)
+        mov     eax, 1                              ; sys_exit
+        int     0x80                                ; syscall
